3eed12f442945d9d54e68a143a9ae46efeaa9ef7
Ivan Kupriyanov <ikupriyanov@horis.ru>
2023-08-22 13:32:45 +0200

    Add a copyright notice to files

--- skia-awt/src/main/kotlin/org/jetbrains/letsPlot/skia/awt/view/SvgPanel.kt
+++ skia-awt/src/main/kotlin/org/jetbrains/letsPlot/skia/awt/view/SvgPanel.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2023 JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
 package org.jetbrains.letsPlot.skia.awt.view
 
 import org.jetbrains.letsPlot.commons.registration.CompositeRegistration
