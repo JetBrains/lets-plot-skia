fb6f7d94c916eecdec7827d941a03a40fa3ea6f3
Ivan Kupriyanov <ikupriyanov@horis.ru>
2024-08-06 21:20:12 +0200

    Repro

---
+++ demo/plot/shared/src/main/kotlin/plotSpec/LabelSpec.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2024 JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package plotSpec
+
+import org.jetbrains.letsPlot.Figure
+import org.jetbrains.letsPlot.geom.geomLabel
+import org.jetbrains.letsPlot.letsPlot
+
+class LabelSpec : PlotDemoSpec {
+    override fun createFigureList(): List<Figure> {
+        val fonts = run {
+            val families = listOf(
+                "Arial",
+                "Calibri",
+                "Garamond",
+                "Geneva",
+                "Georgia",
+                "Helvetica",
+                "Lucida Grande",
+                "Rockwell",
+                "Times New Roman",
+                "Verdana",
+                "sans-serif",
+                "serif",
+                "monospace"
+            )
+            letsPlot() + geomLabel {
+                y = families.indices
+                label = families
+                family = families
+            }
+        }
+
+        return listOf(fonts)
+    }
+}
---
+++ demo/plot/swing/src/main/kotlin/demo/plot/various/Label.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 JetBrains s.r.o.
+ * Use of this source code is governed by the MIT license that can be found in the LICENSE file.
+ */
+
+package demo.plot.various
+
+import demo.util.PlotSpecsDemoWindow
+import plotSpec.LabelSpec
+
+fun main() {
+    with(LabelSpec()) {
+        PlotSpecsDemoWindow(
+            "Labels",
+            createFigureList(),
+        ).open()
+    }
+}
