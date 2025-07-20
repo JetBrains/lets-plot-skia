01ab6b809ff91c3e581079b4de848b781671c61f
Ivan Kupriyanov <ikupriyanov@horis.ru>
2025-07-20 19:05:17 +0200

    Android: fix memory leak

--- platf-skia/src/androidMain/kotlin/org/jetbrains/letsPlot/android/canvas/CanvasView.kt
+++ platf-skia/src/androidMain/kotlin/org/jetbrains/letsPlot/android/canvas/CanvasView.kt
@@ -14,7 +14,6 @@ import org.jetbrains.letsPlot.commons.event.MouseEvent
 import org.jetbrains.letsPlot.commons.event.MouseEventSource
 import org.jetbrains.letsPlot.commons.event.MouseEventSpec
 import org.jetbrains.letsPlot.commons.geometry.Vector
-import org.jetbrains.letsPlot.commons.intern.async.Async
 import org.jetbrains.letsPlot.commons.intern.observable.event.EventHandler
 import org.jetbrains.letsPlot.commons.registration.Registration
 import org.jetbrains.letsPlot.commons.values.Bitmap
@@ -22,10 +21,8 @@ import org.jetbrains.letsPlot.core.canvas.AnimationProvider
 import org.jetbrains.letsPlot.core.canvas.Canvas
 import org.jetbrains.letsPlot.core.canvas.CanvasControl
 import org.jetbrains.letsPlot.core.canvasFigure.CanvasFigure
-import java.util.logging.Logger
+import java.util.*
 
-
-private val LOG = Logger.getLogger("CanvasView")
 
 @SuppressLint("ViewConstructor")
 class CanvasView(context: Context) : View(context) {
@@ -43,10 +40,26 @@ class CanvasView(context: Context) : View(context) {
 
         }
 
-    private val canvasControl = AndroidCanvasControl(context)
+    private val looper = Handler(Looper.getMainLooper())
+    private val animationTimer: Timer = Timer()
+    private val animationUpdateRate: Int = 60
+    private val animationTimerHandlers = ArrayList<(Long) -> Unit>()
+    private val animationTimerTask = object : TimerTask() {
+        override fun run() {
+            looper.post {
+                synchronized(animationTimerHandlers) {
+                    animationTimerHandlers.forEach {
+                        it(System.currentTimeMillis())
+                    }
+                }
+            }
+        }
+    }
+
+    private val canvasControl = AndroidCanvasControl()
     private var figureRegistration: Registration = Registration.EMPTY
     private val sizeListeners = mutableListOf<(Vector) -> Unit>()
-    val mouseEventSource: MouseEventSource = AndroidMouseEventMapper(this, context)
+    private val mouseEventSource: MouseEventSource = AndroidMouseEventMapper(this, context)
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
@@ -59,11 +72,25 @@ class CanvasView(context: Context) : View(context) {
         super.onDraw(canvas)
 
         val contentCanvas = canvasControl.children.last()
-        println(contentCanvas.size)
         canvas.drawBitmap(contentCanvas.platformBitmap, 0f, 0f, null)
         //canvasControl.children.forEach {
         //    canvas.drawBitmap(it.platformBitmap, 0f, 0f, null)
         //}
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        animationTimer.schedule(animationTimerTask, 0L, 1000L / animationUpdateRate)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        figureRegistration.remove()
+        canvasControl.children.clear()
+        animationTimer.cancel()
+        synchronized(animationTimerHandlers) {
+            animationTimerHandlers.clear()
+        }
     }
 
     //override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -82,28 +109,16 @@ class CanvasView(context: Context) : View(context) {
     //    setMeasuredDimension(width, height)
     //}
 
-    inner class AndroidCanvasControl(
-        context: Context,
-    ) : CanvasControl {
-        private val handler = Handler(Looper.getMainLooper())
-        private val animationTimerPeer = AndroidAnimationTimerPeer(executor = { code -> handler.post(code) })
+
+    inner class AndroidCanvasControl : CanvasControl {
         val children = mutableListOf<AndroidCanvas>()
 
-        override fun createCanvas(size: Vector): AndroidCanvas {
-            return AndroidCanvas.create(size, pixelDensity)
-        }
-
-        override fun createSnapshot(bitmap: Bitmap): AndroidSnapshot {
-            return AndroidSnapshot.fromBitmap(bitmap)
-        }
-
-        override fun decodeDataImageUrl(dataUrl: String): Async<Canvas.Snapshot> {
-            TODO("Not yet implemented")
-        }
-
-        override fun decodePng(png: ByteArray, size: Vector): Async<Canvas.Snapshot> {
-            TODO("Not yet implemented")
-        }
+        override fun createCanvas(size: Vector) = AndroidCanvas.create(size, pixelDensity)
+        override fun createSnapshot(bitmap: Bitmap) = AndroidSnapshot.fromBitmap(bitmap)
+        override fun snapshot() = error("Snapshot not supported in AndroidCanvasControl")
+        override fun decodeDataImageUrl(dataUrl: String) = error("decodeDataImageUrl not supported in AndroidCanvasControl")
+        override fun decodePng(png: ByteArray, size: Vector) = error("decodePng not supported in AndroidCanvasControl")
+        override fun <T> schedule(f: () -> T) = error("schedule not supported in AndroidCanvasControl")
 
         override val size: Vector
             get() = Vector(
@@ -111,7 +126,6 @@ class CanvasView(context: Context) : View(context) {
                 (this@CanvasView.height / pixelDensity).toInt()
             )
 
-        //private val mouseEventSource: MouseEventSource = AndroidMouseEventMapper(context)
         override val pixelDensity: Double = context.resources.displayMetrics.density.toDouble()
 
         override fun addChild(index: Int, canvas: Canvas) {
@@ -137,10 +151,6 @@ class CanvasView(context: Context) : View(context) {
             }
         }
 
-        override fun snapshot(): Canvas.Snapshot {
-            TODO("Not yet implemented")
-        }
-
         override fun addEventHandler(
             eventSpec: MouseEventSpec,
             eventHandler: EventHandler<MouseEvent>
@@ -148,14 +158,21 @@ class CanvasView(context: Context) : View(context) {
             return mouseEventSource.addEventHandler(eventSpec, eventHandler)
         }
 
-        override fun <T> schedule(f: () -> T) {
-            TODO("Not yet implemented")
-        }
-
         override fun createAnimationTimer(eventHandler: AnimationProvider.AnimationEventHandler): AnimationProvider.AnimationTimer {
+            // Timer is already running and invokes the `handle` method on animationTimerHandlers
+            // Add the handler to the list to "start" it and remove it to "stop" it.
             return object : AnimationProvider.AnimationTimer {
-                override fun start() = animationTimerPeer.addHandler(::handle)
-                override fun stop() = animationTimerPeer.removeHandler(::handle)
+                override fun start() {
+                    synchronized(animationTimerHandlers) {
+                        animationTimerHandlers.add(::handle)
+                    }
+                }
+
+                override fun stop() {
+                    synchronized(animationTimerHandlers) {
+                        animationTimerHandlers.remove(::handle)
+                    }
+                }
 
                 fun handle(millisTime: Long) {
                     if (eventHandler.onEvent(millisTime)) {
