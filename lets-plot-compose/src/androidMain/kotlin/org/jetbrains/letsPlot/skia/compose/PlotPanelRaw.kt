8aedebba8a08ab5aca4b01e73177b8b006948b0b
Ivan Kupriyanov <ikupriyanov@horis.ru>
2025-09-08 21:32:43 +0200

    Use regular Composable elements instead of SVG to display plot errors on Android

--- lets-plot-compose/src/androidMain/kotlin/org/jetbrains/letsPlot/skia/compose/PlotPanelRaw.kt
+++ lets-plot-compose/src/androidMain/kotlin/org/jetbrains/letsPlot/skia/compose/PlotPanelRaw.kt
@@ -5,14 +5,19 @@
 
 package org.jetbrains.letsPlot.skia.compose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.viewinterop.AndroidView
 import org.jetbrains.letsPlot.android.canvas.CanvasView
 import org.jetbrains.letsPlot.commons.logging.PortableLogging
+import org.jetbrains.letsPlot.core.spec.config.PlotConfig
+import org.jetbrains.letsPlot.core.util.MonolithicCommon.processRawSpecs
+import org.jetbrains.letsPlot.core.util.PlotThemeHelper
 import org.jetbrains.letsPlot.core.util.sizing.SizingPolicy
-import org.jetbrains.letsPlot.raster.builder.MonolithicCanvas
 import org.jetbrains.letsPlot.raster.view.PlotCanvasFigure
 
 //import org.jetbrains.letsPlot.skia.compose.util.NaiveLogger
@@ -32,23 +37,64 @@ actual fun PlotPanelRaw(
 ) {
     var plotCanvasFigure by remember { mutableStateOf(PlotCanvasFigure()) }
 
-//    LOG.info { "Recompose PlotPanel()" }
+    // Cache processed plot spec to avoid reprocessing the same raw spec on every recomposition.
 
-    AndroidView(
-        factory = { ctx ->
-//            LOG.info { "PlotPanel: AndroidView factory called" }
-            val canvasView = CanvasView(ctx)
-            canvasView.figure = plotCanvasFigure
-            canvasView
-        },
-        modifier = modifier,
-        update = { canvasView ->
-            MonolithicCanvas.updatePlotFigureFromRawSpec(
-                plotCanvasFigure = plotCanvasFigure,
-                rawSpec = rawSpec,
-                sizingPolicy = SizingPolicy.fitContainerSize(preserveAspectRatio),
-                computationMessagesHandler = computationMessagesHandler
-            )
+    // Note: Use remember(rawSpec.hashCode()), to bypass the equality check and use the content hash directly.
+    // The issue was that remember(rawSpec) uses some kind of comparison (equals()?) which somehow not working for `MutableMap`.
+    val processedPlotSpec = remember(rawSpec.hashCode()) {
+        processRawSpecs(rawSpec, frontendOnly = false)
+    }
+
+    val showErrorMessage = PlotConfig.isFailure(processedPlotSpec)
+
+    println("PlotPanel: recomposition")
+
+    // Background
+    val finalModifier = if (showErrorMessage) {
+        modifier.background(Color.LightGray)
+    } else {
+        if (containsBackground(modifier)) {
+            // Do not change the user-defined background
+            modifier
+        } else {
+            // Use background color from the plot theme
+            val lpColor = PlotThemeHelper.plotBackground(processedPlotSpec)
+            val lpBackground = Color(lpColor.red, lpColor.green, lpColor.blue, lpColor.alpha)
+            modifier.background(lpBackground)
         }
-    )
+    }
+
+//    LOG.info { "Recompose PlotPanel()" }
+    if (showErrorMessage) {
+        // Show error message
+        plotCanvasFigure.update(processedPlotSpec, SizingPolicy.fitContainerSize(preserveAspectRatio), computationMessagesHandler)
+        BasicTextField(
+            value = PlotConfig.getErrorMessage(processedPlotSpec),
+            onValueChange = { },
+            readOnly = true,
+            textStyle = errorTextStyle,
+            modifier = errorModifier
+        )
+    } else {
+        @Suppress("COMPOSE_APPLIER_CALL_MISMATCH") // Gemini says that this is a false positive
+        AndroidView(
+            factory = { ctx ->
+            println("PlotPanel: AndroidView factory called")
+                val canvasView = CanvasView(ctx)
+                canvasView.figure = plotCanvasFigure
+                canvasView
+            },
+            modifier = finalModifier,
+            update = { _ ->
+                println("PlotPanel: AndroidView update called")
+                plotCanvasFigure.update(processedPlotSpec, SizingPolicy.fitContainerSize(preserveAspectRatio), computationMessagesHandler)
+            }
+        )
+    }
+}
+
+private fun containsBackground(modifier: Modifier): Boolean {
+    return modifier.foldIn(false) { hasBg, element ->
+        hasBg || element.toString().contains("BackgroundElement")
+    }
 }
